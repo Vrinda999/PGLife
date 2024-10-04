391395999be5ca42b187fe65009f469509b2dc9d
Vrinda <abrolvrinda@gmail.com>
2024-10-04 16:25:00 +0530

    feat: login modal with db connection

--- api/login_submit.php
+++ api/login_submit.php
@@ -1,1 +1,28 @@
-require("../includes/database_connect.php")
+<?php
+session_start();
+require("../includes/daabase_connect.php");
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+$password = sha1($password);
+
+$sql = "SELECT * FROM users WHERE email='$email' ANd password = '$password'";
+$result = mysqli_query($conn, $sql);
+if (!$result) {
+    echo "Something Went Wrong!";
+    exit;
+}
+
+$row_count = mysqli_num_rows($result);
+if ($row_count == 0) {
+    echo "Login Failed! Invalid Wmail or Password :(";
+    exit;
+}
+
+$row = mysqli_fetch_assoc($result);
+$_SESSION['user_id'] = $row['id'];
+$_SESSION['full_name'] = $row['full_name'];
+$_SESSION['email'] = $row['email'];
+
+header('location: ../index.php');
+mysqli_close($conn);
