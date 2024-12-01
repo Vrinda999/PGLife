f0aed0e1676acc3f941ecb209970e6e86dc64c1b
Vrinda <abrolvrinda@gmail.com>
2024-12-01 23:43:22 +0530

    feat: No Property Page

---
+++ react-app/src/1/NoProperty.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NoProperty = () => {
+
+    return (
+        <div className="no-property-container">
+            <p>
+                No PG to List.
+            </p>
+        </div>
+    );
+}
+
+export default NoProperty;
