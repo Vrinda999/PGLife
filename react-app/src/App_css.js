c513d2a336007d7e1804226ddc71308836e5f868
Vrinda <abrolvrinda@gmail.com>
2024-12-02 02:32:52 +0530

    minor changes

--- react-app/src/App_css.js
+++ react-app/src/App_css.js
@@ -1,8 +1,6 @@
 import FilterBar from './1/FilterBar'
 import FilterModal from './1/FilterModal';
 import PropertyCard from './1/PropertyCard';
-import NoProperty from './NoProperty';
-import { base_path } from './1/utils';
 
 const App = () => {
     return (
